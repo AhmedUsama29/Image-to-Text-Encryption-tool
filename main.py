a9b8a3aeb78a3d29844e8ebfd56fe35af4cc77b2
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2024-11-04 00:36:11 +0200

    fixed some bugs

--- main.py
+++ main.py
@@ -15,20 +15,30 @@ def show_main_page():
 
 # Initialize the root window
 root = tk.Tk()
-root.title("IMAGE <=> TEXT")
+root.title("Image to Text Encryption")
 root.geometry("800x500")
 root.configure(bg='black')
 
 # Main Frame
 main_frame = tk.Frame(root, bg='black')
-title_label = tk.Label(main_frame, text="IMAGE <=> TEXT", font=("Arial", 24), fg="white", bg="black")
-title_label.pack(pady=20)
+main_frame.pack(fill="both", expand=True, padx=50, pady=50)  # Add padding to center the main frame
 
-# Encrypt and Decrypt Buttons on Main Page
-encrypt_button = tk.Button(main_frame, text="Encrypt", font=("Arial", 14), bg="#3d85c6", fg="white", command=show_encrypt_page)
-decrypt_button = tk.Button(main_frame, text="Decrypt", font=("Arial", 14), bg="#3d85c6", fg="white", command=show_decrypt_page)
-encrypt_button.pack(pady=10)
-decrypt_button.pack(pady=10)
+title_label = tk.Label(main_frame, text="From Pixels to Cipher", font=("Dragon Hunter", 30 ), fg="white", bg="black")
+title_label.pack(pady=50)
+
+# Encrypt and Decrypt Buttons on Main Page (positioned side by side and centered)
+button_frame = tk.Frame(main_frame, bg='black')
+button_frame.pack(pady=10)
+
+encrypt_button = tk.Button(button_frame, text="Encrypt", font=("Times New Roman", 16), width=15, height=3, bg="#3d85c6", fg="white", command=show_encrypt_page)
+decrypt_button = tk.Button(button_frame, text="Decrypt", font=("Times New Roman", 16), width=15, height=3, bg="#3d85c6", fg="white", command=show_decrypt_page)
+
+encrypt_button.grid(row=0, column=0, padx=100)  # زودنا المسافة الأفقية هنا
+decrypt_button.grid(row=0, column=1, padx=100)  # زودنا المسافة الأفقية هنا
+
+
+# Center `button_frame` within `main_frame`
+button_frame.pack(expand=True)
 
 # Encrypt Frame
 encrypt_frame = tk.Frame(root, bg='black')
@@ -47,7 +57,6 @@ secret_key_entry_encrypt.insert(0, "Enter Secret Key for Encryption")
 # Encrypt Button
 encrypt_button_frame = tk.Button(encrypt_frame, text="Encrypt", bg='#3d85c6', fg='white', font=('Times New Roman', 14))
 encrypt_button_frame.pack(pady=10)
-
 
 # Textbox for input text to encrypt
 text_input_encrypt = tk.Text(encrypt_frame, wrap="word", width=40, height=10, font=("Arial", 12))
@@ -79,7 +88,7 @@ decrypt_button_frame.pack(pady=10)
 
 # Back Button on Decrypt Page (Positioned at the bottom left)
 back_button_decrypt = tk.Button(decrypt_frame, text="Back", command=show_main_page, bg='#3d85c6', fg='white', font=('Times New Roman', 20))
-back_button_decrypt.place(x=10, rely=1.0, anchor="sw")  # Adjust 'y' to fine-tune the bottom spacing
+back_button_decrypt.place(x=10, rely=1.0, anchor="sw")
 
 # Show the main page initially
 main_frame.pack(fill="both", expand=True)
